202e0fbdd9cd4ce5f0891c910b27969b6036851d
pshkltsk <pshkltsk@gmail.com>
2023-07-03 17:24:49 +0300

    INITIAL

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Pavel Kylitsky on 03/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
